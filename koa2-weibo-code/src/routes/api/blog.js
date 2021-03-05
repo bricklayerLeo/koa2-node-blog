af665e72419812fb78d454c86632308c4d5e6a5e
bricklayerLeo <873634131@qq.com>
2021-03-05 19:28:17 +0800

    bug

--- koa2-weibo-code/src/routes/api/blog.js
+++ koa2-weibo-code/src/routes/api/blog.js
@@ -1,12 +1,12 @@
 const router = require('koa-router')()
 const loginCheck = require('../../middlewares/loginCheck')
 const blogValidate = require('../../validate/blog')
-const genValidate = require('../../middlewares/validate')
+const { genValidator } = require('../../middlewares/validate')
 const { createBloCon } = require('../../controller/blog')
 
 router.prefix('/api/blog')
 
-router.post('/create', loginCheck, genValidate(blogValidate), async (ctx, next) => {
+router.post('/create', loginCheck, genValidator(blogValidate), async (ctx, next) => {
     const { image, content } = ctx.request.body
     ctx.body = await createBloCon(ctx, { image, content })
 })
